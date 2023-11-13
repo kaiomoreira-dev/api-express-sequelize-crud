d30bea353957827a79d799d0f736632bfff3f429
Kaio Moreira <kaio-dev@outlook.com>
2023-11-13 13:20:39 -0300

    create: db for test and production

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -5,5 +5,19 @@ module.exports = {
     database: 'database_development',
     host: '127.0.0.1',
     dialect: 'postgres',
+  },
+  test: {
+    username: 'kaiodev',
+    password: 'docker123',
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    username: 'kaiodev',
+    password: 'docker123',
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'postgres',
   }
 }
